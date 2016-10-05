8b6ceca81aed59a2b0bf83b804ab511c5613a0f4
nlkassad <nlkassad@hotmail.com>
2016-10-05 04:37:29 -0400

    Bundle Changes

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(60);
+	module.exports = __webpack_require__(63);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 60:
+/***/ 63:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
